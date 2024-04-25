7c7d7c32eefd30073b9821b8470844ab89b783b4
aryan-1078 <139322380+aryan-1078@users.noreply.github.com>
2024-04-26 01:14:46 +0530

    Update preemptive_priority.c

--- Scheduling/preemptive_priority.c
+++ Scheduling/preemptive_priority.c
@@ -7,6 +7,7 @@ void priority_preemptive(int n, int processes[], int arrival_time[], int burst_t
     int wait_time[n];
     int current_time = 0;
     int completed = 0;
+    int total_turnaround_time = 0, total_wait_time = 0;
 
     for (int i = 0; i < n; i++) {
         remaining_time[i] = burst_time[i];
@@ -33,6 +34,9 @@ void priority_preemptive(int n, int processes[], int arrival_time[], int burst_t
                 completion_time[highest_priority_process] = current_time + 1;
                 turnaround_time[highest_priority_process] = completion_time[highest_priority_process] - arrival_time[highest_priority_process];
                 wait_time[highest_priority_process] = turnaround_time[highest_priority_process] - burst_time[highest_priority_process];
+                total_turnaround_time += turnaround_time[highest_priority_process];
+                total_wait_time += wait_time[highest_priority_process];
+                
             }
             current_time++;
         }
@@ -41,8 +45,12 @@ void priority_preemptive(int n, int processes[], int arrival_time[], int burst_t
     // Display process table
     printf("Process\tArrival Time\tBurst Time\tPriority\tCompletion Time\tTurnaround Time\tWait Time\n");
     for (int i = 0; i < n; i++) {
-        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t\t%d\t\t\t%d\n", processes[i], arrival_time[i], burst_time[i], priority[i], completion_time[i], turnaround_time[i], wait_time[i]);
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t\t%d\t\t\t%d\n", i+1, arrival_time[i], burst_time[i], priority[i], completion_time[i], turnaround_time[i], wait_time[i]);
     }
+    float avg_tat = (float)total_turnaround_time / n;
+    float avg_wt = (float)total_wait_time / n;
+    printf("\navg tat is %.2f\n", avg_tat);
+    printf("\navg wt is %.2f\n", avg_wt);
 }
 
 int main() {
@@ -57,13 +65,8 @@ int main() {
 
     printf("Enter arrival time, burst time, and priority for each process:\n");
     for (int i = 0; i < n; i++) {
-        printf("Process %d Arrival Time: ", i + 1);
-        scanf("%d", &arrival_time[i]);
-        printf("Process %d Burst Time: ", i + 1);
-        scanf("%d", &burst_time[i]);
-        printf("Process %d Priority: ", i + 1);
-        scanf("%d", &priority[i]);
-        processes[i] = i + 1; // Process IDs
+        printf("process %d: ", i+1);
+        scanf("%d %d %d", &arrival_time[i], &burst_time[i], &priority[i]);
     }
 
     priority_preemptive(n, processes, arrival_time, burst_time, priority);
