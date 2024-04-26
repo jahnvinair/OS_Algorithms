c0c3f8031bef6cd18b3703a924f1ef1e8b289f2f
aryan-1078 <139322380+aryan-1078@users.noreply.github.com>
2024-04-27 01:24:32 +0530

    Create banker's_algorithm.c

---
+++ Scheduling/banker's_algorithm.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// Function prototypes
+bool isSafe(int available[], int maxm[][3], int allocation[][3], int numProcesses, int numResources);
+int findAvailableProcess(int available[], int maxm[][3], int allocation[][3], int numProcesses, int numResources);
+
+int main() {
+    int numProcesses, numResources;
+    printf("Enter the number of processes: ");
+    scanf("%d", &numProcesses);
+    printf("Enter the number of resource types: ");
+    scanf("%d", &numResources);
+
+    int available[numResources];
+    printf("Enter the available resources (separated by spaces): ");
+    for (int i = 0; i < numResources; i++) {
+        scanf("%d", &available[i]);
+    }
+
+    int (*maxm)[numResources] = malloc(numProcesses * numResources * sizeof(int));
+    printf("Enter the maximum requirements for each process:\n");
+    for (int i = 0; i < numProcesses; i++) {
+        printf("Process %d: ", i);
+        for (int j = 0; j < numResources; j++) {
+            scanf("%d", &maxm[i][j]);
+        }
+    }
+
+    int (*allocation)[numResources] = malloc(numProcesses * numResources * sizeof(int));
+    printf("Enter the allocated resources for each process:\n");
+    for (int i = 0; i < numProcesses; i++) {
+        printf("Process %d: ", i);
+        for (int j = 0; j < numResources; j++) {
+            scanf("%d", &allocation[i][j]);
+        }
+    }
+
+    if (isSafe(available, maxm, allocation, numProcesses, numResources)) {
+        printf("The system is in a safe state.\n");
+    } else {
+        printf("The system is in an unsafe state and may lead to a deadlock.\n");
+    }
+
+    free(maxm);
+    free(allocation);
+
+    return 0;
+}
+
+bool isSafe(int available[], int maxm[][3], int allocation[][3], int numProcesses, int numResources) {
+    int work[numResources]; // Work vector
+    bool *finish = calloc(numProcesses, sizeof(bool)); // Finish vector
+    int safeSequence[numProcesses]; // Safe sequence
+    int count = 0; // Counter for safe sequence
+
+    // Copy available resources to work vector
+    for (int i = 0; i < numResources; i++) {
+        work[i] = available[i];
+    }
+
+    // Find safe sequence
+    while (count < numProcesses) {
+        bool found = false;
+        int processIndex = findAvailableProcess(work, maxm, allocation, numProcesses, numResources);
+
+        if (processIndex != -1) {
+            for (int i = 0; i < numResources; i++) {
+                work[i] += allocation[processIndex][i];
+            }
+            safeSequence[count++] = processIndex;
+            finish[processIndex] = true;
+            found = true;
+        }
+
+        if (!found) {
+            break;
+        }
+    }
+
+    // Check if all processes can be finished
+    bool isSafe = (count == numProcesses);
+
+    // Print safe sequence
+    if (isSafe) {
+        printf("Safe sequence: ");
+        for (int i = 0; i < numProcesses; i++) {
+            printf("%d ", safeSequence[i]); // Print the actual process IDs
+        }
+        printf("\n");
+    }
+
+    free(finish);
+    return isSafe;
+}
+
+int findAvailableProcess(int available[], int maxm[][3], int allocation[][3], int numProcesses, int numResources) {
+    for (int i = 0; i < numProcesses; i++) {
+        bool canAllocate = true;
+        for (int j = 0; j < numResources; j++) {
+            if (maxm[i][j] - allocation[i][j] > available[j]) {
+                canAllocate = false;
+                break;
+            }
+        }
+        if (canAllocate) {
+            return i;
+        }
+    }
+    return -1;
+}
